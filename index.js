4da729843af8d9cb23709d5102a5cfded7f7b585
Nadella Himavanth <158074964+Himavanth2384@users.noreply.github.com>
2024-08-25 12:03:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const user_data = {
+  user_id: "nadella_himavanth_21BEC2184",
+  email: "nadella.himavanth2021@vitstudent.ac.in",
+  roll_number: "21BEC2184",
+};
+
+app.get("/", (req, res) => {
+  res.send("API is up and running! Nadella Himavanth ( 21BEC2184 )");
+});
+
+app.get("/bfhl", (req, res) => {
+  res.json({ operation_code: "XYZ123" });
+});
+
+app.post("/bfhl", (req, res) => {
+  const data = req.body.data;
+  const numbers = [];
+  const alphabets = [];
+  let highestLowercase = null;
+
+  data.forEach((item) => {
+    if (!isNaN(item)) {
+      numbers.push(item.toString());
+    } else if (
+      typeof item === "string" &&
+      item.length === 1 &&
+      /[a-zA-Z]/.test(item)
+    ) {
+      alphabets.push(item);
+      if (item === item.toLowerCase()) {
+        if (!highestLowercase || item > highestLowercase) {
+          highestLowercase = item;
+        }
+      }
+    }
+  });
+
+  res.json({
+    is_success: true,
+    user_id: user_data.user_id,
+    email: user_data.email,
+    roll_number: user_data.roll_number,
+    numbers: numbers,
+    alphabets: alphabets,
+    highest_lowercase_alphabet: highestLowercase ? [highestLowercase] : [],
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
